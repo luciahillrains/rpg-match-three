c7d5e2d4e5c89da71519131bcf78cced99b64867
luciahillrains <luciahillrains@gmail.com>
2018-05-14 11:58:40 -0500

    filled out tile component

--- src/app/board-tile/board-tile.component.ts
+++ src/app/board-tile/board-tile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-board-tile',
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board-tile.component.css']
 })
 export class BoardTileComponent implements OnInit {
+	@Input()
+	value:number = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  generateClass() {
+  	let clz = "black";
+  	switch(this.value) {
+  		case 1:
+  			clz = "red";
+  			break;
+  		case 2:
+  			clz = "blue";
+  			break;
+  		case 3:
+  			clz = "green";
+  			break;
+  		case 4:
+  			clz = "brown";
+  			break;
+  		case 5:
+  			clz = "yellow";
+  			break;
+  	}
+  	return clz;
+
   }
 
 }
