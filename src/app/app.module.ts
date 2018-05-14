942a266ccce28ed1f976d3546be8d2537bbaefd8
luciahillrains <luciahillrains@gmail.com>
2018-05-14 11:49:16 -0500

    added tile and board components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardTileComponent } from './board-tile/board-tile.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardTileComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/board-tile/board-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board-tile',
+  templateUrl: './board-tile.component.html',
+  styleUrls: ['./board-tile.component.css']
+})
+export class BoardTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
