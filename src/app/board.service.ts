0a8671006e955a1351bd3cdd01c7da5b7a98be4c
luciahillrains <luciahillrains@gmail.com>
2018-05-14 11:47:38 -0500

    added board service

---
+++ src/app/board.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+	BOARD_SIZE:number = 8;
+	board:number[][];
+
+  constructor() {
+  	this.generateBoard();
+  }
+
+  generateBoard() {
+  	this.board = [];
+  	for(let i = 0; i < this.BOARD_SIZE; i++) {
+  		this.board[i] = [];
+  		for(let j = 0; j < this.BOARD_SIZE; j++) {
+  			this.board[i][j] = 0;
+  		}
+  	}
+  }
+}
